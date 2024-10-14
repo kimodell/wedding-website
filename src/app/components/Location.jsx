9720f9897fa410b00365f7a20dc7fbd02b0978b8
Kim O'Dell <kimbo.odell@gmail.com>
2024-10-14 16:24:43 -0400

    Add style for location componenent

--- src/app/components/Location.jsx
+++ src/app/components/Location.jsx
@@ -1,29 +1,27 @@
-import styles from "../styles/Location.module.css"
+import styles from "../styles/Location.module.css";
 import Image from "next/image";
-import oast from "../images/oast.png"
+import oast from "../images/oast.png";
 import Map from "./Map";
 
-export default function Location () {
+export default function Location() {
   return (
     <div id="location">
       <h2>Location</h2>
-      <h4>
-        Niagara Oast House Brewers
-      </h4>
-      <p>
-        2017 Niagara Stone
-      </p>
-      <p>
-        Niagara-on-the-Lake, ON L0S 1J0
-      </p>
-      <Image
-        src={oast}
-        alt="Niagara Oast House Brewers"
-        width={500}
-        height={300}
-      />
-      <Map/>
-
+      <div className={styles.locationContent}>
+        <div className={styles.locationText}>
+          <h4>Niagara Oast House Brewers</h4>
+          <p>2017 Niagara Stone</p>
+          <p>Niagara-on-the-Lake, ON L0S 1J0</p>
+        </div>
+        <Image
+          className={styles.locationImage}
+          src={oast}
+          alt="Niagara Oast House Brewers"
+          width={500}
+          height={300}
+        />
+      </div>
+      <Map className={styles.locationMap} />
     </div>
   );
 }
