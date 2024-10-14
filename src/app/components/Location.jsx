94611a33272761598f96dd48b2cc09ed29248dd5
Kim O'Dell <kimbo.odell@gmail.com>
2024-10-14 13:58:36 -0400

    Add location details and image

--- src/app/components/Location.jsx
+++ src/app/components/Location.jsx
@@ -1,12 +1,27 @@
 import styles from "../styles/Location.module.css"
+import Image from "next/image";
+import oast from "../images/oast.png"
 
 export default function Location () {
   return (
     <div id="location">
       <h2>Location</h2>
+      <h4>
+        Niagara Oast House Brewers
+      </h4>
       <p>
-        Where section! Coming soon!
+        2017 Niagara Stone
       </p>
+      <p>
+        Niagara-on-the-Lake, ON L0S 1J0
+      </p>
+      <Image
+        src={oast}
+        alt="Niagara Oast House Brewers"
+        width={500}
+        height={300}
+      />
+
     </div>
   );
 }
