0c960c1f12308694f4255bc1b1de9cb3280d4014
Kim O'Dell <kimbo.odell@gmail.com>
2024-11-10 14:42:54 -0500

    Modify placement and width of map componenet for larger viewports

--- src/app/components/Location.jsx
+++ src/app/components/Location.jsx
@@ -10,8 +10,8 @@ export default function Location() {
       <div className={styles.locationContent}>
         <div className={styles.locationText}>
           <h4>Niagara Oast House Brewers</h4>
-          <p>2017 Niagara Stone</p>
-          <p>Niagara-on-the-Lake, ON L0S 1J0</p>
+          <h4>2017 Niagara Stone</h4>
+          <h4>Niagara-on-the-Lake, ON L0S 1J0</h4>
           <br/>
           <p><em>Please note: Parking for our guests is located in the overflow lot to the Southwest of the brewery.
           If you follow directions like Kim, this can be found on the right hand side when facing the front of the big red barn.</em></p>
