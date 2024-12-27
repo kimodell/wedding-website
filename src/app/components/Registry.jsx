2ac7263ce48af975a9829710ae0236214689bbb2
Kim O'Dell <kimbo.odell@gmail.com>
2024-12-27 13:48:33 -0500

    Modify style for registry component

--- src/app/components/Registry.jsx
+++ src/app/components/Registry.jsx
@@ -5,9 +5,15 @@ export default function Registry() {
     <div id="registry">
       <h2>Gifts</h2>
       <div className={styles.registry}>
-        <p className={styles.registryParagraph}>We are so grateful to have you join us in celebrating our special day. Your presence alone is the greatest gift we could ask for!</p>
-        <p className={styles.registryParagraph}>If you would like to honor us with a gift, we kindly ask for monetary contributions in place of a registry. This will help us create lasting memories as we start this new chapter of our story together.</p>
-        <p className={styles.registryParagraph}>Thank you for your love and support - we can't wait to celebrate with you!</p>
+        <p className={styles.registryParagraph}>
+          We are so grateful to have you join us in celebrating our special day. Your presence alone is the greatest gift we could ask for!
+        </p>
+        <p className={styles.registryParagraph}>
+          If you would like to honor us with a gift, we kindly ask for monetary contributions in place of a registry. This will help us create lasting memories as we start this new chapter of our story together.
+        </p>
+        <p className={styles.registryParagraph}>
+          Thank you for your love and support - we can't wait to celebrate with you!
+        </p>
       </div>
     </div>
   );
